50dd8dfcdf02f837223d6b5e49049f0e27c3cb44
arswe <abdurrahmansoftw@gmail.com>
2024-03-12 10:58:22 +0600

    add tooltip

---
+++ components/ui/light-dark-toggle.tsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+
+const LightDarkToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true)
+  return <div>LightDarkToggle</div>
+}
+
+export default LightDarkToggle
