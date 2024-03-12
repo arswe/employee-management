e9d83936fcf2fe3c22f1d6e6643e3b99b3e49856
arswe <abdurrahmansoftw@gmail.com>
2024-03-12 10:41:56 +0600

    LandingPage add button

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,12 +1,22 @@
 import { Button } from '@/components/ui/button'
+import { Fragment } from 'react'
+
+import { PersonStandingIcon } from 'lucide-react'
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Support me</h1>
-      <Button> Login In</Button>
-      <Button> Sign Up</Button>
-    </div>
+    <Fragment>
+      <h1 className='flex gap-2 items-center'>
+        <PersonStandingIcon size={50} className='text-pink-400' /> Support me
+      </h1>
+      <p>
+        best dashboard for your personal customer support. <br />
+      </p>
+      <div className='flex gap-2 items-center'>
+        <Button> Login In</Button>
+        <Button variant={'outline'}> Sign Up</Button>
+      </div>
+    </Fragment>
   )
 }
 
