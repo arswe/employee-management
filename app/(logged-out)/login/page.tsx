f7bf8ca0dd980498ae67f98e5bab01e67ebfa65d
arswe <abdurrahmansoftw@gmail.com>
2024-03-12 11:38:33 +0600

    basics LoginPage UI

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -1,5 +1,38 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { PersonStandingIcon } from 'lucide-react'
+import Link from 'next/link'
+
 const LoginPage = () => {
-  return <div>LoginPage</div>
+  return (
+    <>
+      <PersonStandingIcon size={50} />
+      <Card className='w-full max-w-sm'>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to your Support me account</CardDescription>
+          <CardContent></CardContent>
+        </CardHeader>
+
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter className='flex justify-between'>
+          <small> Do not have an account?</small>
+          <Button asChild variant='outline' size='sm'>
+            <Link href='/sign-Up'>Sign Up</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  )
 }
 
 export default LoginPage
