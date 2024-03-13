a6137769c58e708cd52007b1bc71cdb5180a0038
arswe <abdurrahmansoftw@gmail.com>
2024-03-13 14:39:23 +0600

    MainMenu refactoring

--- components/main-menu.tsx
+++ components/main-menu.tsx
@@ -6,10 +6,10 @@ import LightDarkToggle from './ui/light-dark-toggle'
 
 const MainMenu = () => {
   return (
-    <div className='bg-muted overflow-auto p-4 flex flex-col'>
-      <div className='border-b dark:border-black border-b-zinc-50 pb-4'>
+    <nav className='bg-muted overflow-auto p-4 flex flex-col'>
+      <header className='border-b dark:border-black border-b-zinc-50 pb-4'>
         <MenuTitle />
-      </div>
+      </header>
 
       <div className='py-4 grow'>
         <MenuItem href='/dashboard'>My Dashboard</MenuItem>
@@ -19,7 +19,7 @@ const MainMenu = () => {
         <MenuItem href='/dashboard/settings'>Settings</MenuItem>
       </div>
 
-      <div className='flex gap-2 items-center'>
+      <footer className='flex gap-2 items-center'>
         <Avatar>
           <AvatarFallback className='bg-pink-300 dark:bg-pink-800'>TP</AvatarFallback>
         </Avatar>
@@ -28,8 +28,8 @@ const MainMenu = () => {
         </Link>
 
         <LightDarkToggle className='ml-auto' />
-      </div>
-    </div>
+      </footer>
+    </nav>
   )
 }
 
