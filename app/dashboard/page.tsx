ca39933cb64acecebe8428ab3af24297d3e177c3
arswe <abdurrahmansoftw@gmail.com>
2024-03-14 00:53:46 +0600

    Tabs add to state

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <Tabs defaultValue='employees'>
+      <TabsList>
+        <TabsTrigger value='employees'>Employees State</TabsTrigger>
+        <TabsTrigger value='teams'>Teams State</TabsTrigger>
+      </TabsList>
+      <TabsContent value='employees'>Employee State view</TabsContent>
+      <TabsContent value='teams'>Teams State view</TabsContent>
+    </Tabs>
   )
 }
 
