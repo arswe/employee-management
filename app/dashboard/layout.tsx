22e4549f5d6c6b6f2a939edfc2544645d278a91a
arswe <abdurrahmansoftw@gmail.com>
2024-03-13 11:56:13 +0600

    MainMenu

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,14 @@
+import MainMenu from '@/components/main-menu'
 import React from 'react'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='grid grid-cols-[250px_1fr] h-screen'>
-      <div className='bg-muted overflow-auto'>side panel</div>
-      <div>{children}</div>
+      <MainMenu />
+      <div className='overflow-auto py-2 px-4'>
+        <h1 className='pb-4'> Welcome back, AbdurRahman</h1>
+        {children}
+      </div>
     </div>
   )
 }
---
+++ components/main-menu.tsx
@@ -0,0 +1,5 @@
+const MainMenu = () => {
+  return <div className='bg-muted overflow-auto p-4'>side panel</div>
+}
+
+export default MainMenu
