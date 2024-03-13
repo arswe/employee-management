ce68e8347fea308912c1861bffed66ac0638d789
arswe <abdurrahmansoftw@gmail.com>
2024-03-13 11:51:14 +0600

    overflow-auto

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='grid grid-cols-[250px_1fr] h-screen'>
-      <div className='bg-muted'>side panel</div>
+      <div className='bg-muted overflow-auto'>side panel</div>
       <div>{children}</div>
     </div>
   )
