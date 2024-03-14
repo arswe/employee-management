284130817ce8c196e16905c5935603f9b466ac32
arswe <abdurrahmansoftw@gmail.com>
2024-03-14 17:01:26 +0600

    radius

--- components/employees/word-location-trends.tsx
+++ components/employees/word-location-trends.tsx
@@ -1,6 +1,6 @@
 import { data } from '@/data/data'
 
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 const WordLocationTrends = () => {
   return (
@@ -8,8 +8,9 @@ const WordLocationTrends = () => {
       <BarChart data={data}>
         <XAxis dataKey={'name'} stroke='#888888' />
         <YAxis stroke='#888888' />
+        <Tooltip />
         <Bar dataKey='office' stackId={1} fill='#ec4899' />
-        <Bar dataKey='wfh' stackId={1} fill='#6b7280' />
+        <Bar dataKey='wfh' stackId={1} fill='#6b7280' radius={[4, 4, 0, 0]} />
       </BarChart>
     </ResponsiveContainer>
   )
