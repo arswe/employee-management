0f63586a7e5b660441902239825b1eab7872ad6c
arswe <abdurrahmansoftw@gmail.com>
2024-03-14 22:50:42 +0600

    labelClassName  wrapperClassName

--- components/teams/team-distribution-chart.tsx
+++ components/teams/team-distribution-chart.tsx
@@ -5,7 +5,10 @@ const TeamDistributionChart = () => {
   return (
     <ResponsiveContainer width={'100%'} height={150}>
       <PieChart>
-        <Tooltip />
+        <Tooltip
+          labelClassName='font-bold'
+          wrapperClassName='!text-sm dark:!bg-black rounded-md dark:!border-border'
+        />
         <Pie dataKey='value' nameKey={'name'} data={teamPieData}>
           {teamPieData.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={entry.color} />
