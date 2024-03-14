bc7da53d55de35699bb11f653b697ea643f71605
arswe <abdurrahmansoftw@gmail.com>
2024-03-14 22:54:14 +0600

    TeamDistributionChart

--- components/teams/team-distribution-chart.tsx
+++ components/teams/team-distribution-chart.tsx
@@ -3,18 +3,23 @@ import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts'
 
 const TeamDistributionChart = () => {
   return (
-    <ResponsiveContainer width={'100%'} height={150}>
+    <ResponsiveContainer width='100%' height={150}>
       <PieChart>
         <Tooltip
           labelClassName='font-bold'
-          wrapperClassName='!text-sm dark:!bg-black rounded-md dark:!border-border'
+          wrapperClassName='dark:[&_.recharts-tooltip-item]:!text-white [&_.recharts-tooltip-item]:!text-black !text-sm dark:!bg-black rounded-md dark:!border-border'
         />
-        <Pie dataKey='value' nameKey={'name'} data={teamPieData}>
-          {teamPieData.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={entry.color} />
+        <Pie
+          data={teamPieData}
+          dataKey='value'
+          nameKey='name'
+          labelLine={false}
+          label
+        >
+          {teamPieData.map((dataItem, i) => (
+            <Cell key={i} fill={dataItem.color} />
           ))}
         </Pie>
-        <Tooltip />
       </PieChart>
     </ResponsiveContainer>
   )
