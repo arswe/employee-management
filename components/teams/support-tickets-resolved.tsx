976a839515e571ff4a81f36819d7afbaf9ec9e28
arswe <abdurrahmansoftw@gmail.com>
2024-03-14 23:06:22 +0600

    Legend

--- components/teams/support-tickets-resolved.tsx
+++ components/teams/support-tickets-resolved.tsx
@@ -18,8 +18,8 @@ const SupportTicketsResolved = () => {
           labelClassName='font-bold'
           wrapperClassName='!text-sm dark:!bg-black rounded-md dark:!border-border'
         />
-        <XAxis  dataKey='name' stroke='#888888' />
-        <YAxis  stroke='#888888' />
+        <XAxis dataKey='name' stroke='#888888' />
+        <YAxis stroke='#888888' />
         <CartesianGrid strokeDasharray='3 3' />
         <Line type='monotone' dataKey='delta' stroke='#84cc16' />
         <Line type='monotone' dataKey='alpha' stroke='#3b82f6' />
