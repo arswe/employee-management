3bf0d53c493fe0fdddb9b4ee467fdde529d4f09c
arswe <abdurrahmansoftw@gmail.com>
2024-03-20 00:06:16 +0600

    EmployeesPage

--- app/dashboard/employees/page.tsx
+++ app/dashboard/employees/page.tsx
@@ -1,79 +1,10 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { DataTable } from '@/components/ui/data-table'
-import { setTimeout } from 'timers/promises'
-import { columns, Employee } from './columns'
+import { employees } from '@/data/empData'
+import { columns } from './columns'
 
 const EmployeesPage = async () => {
-  await setTimeout(5000)
-  const employees: Employee[] = [
-    {
-      id: 1,
-      firstName: 'Colin',
-      lastName: 'Murray',
-      teamName: 'alpha',
-      isTeamLeader: true,
-      avatar: '',
-    },
-    {
-      id: 2,
-      firstName: 'Tom',
-      lastName: 'Phillips',
-      teamName: 'alpha',
-      isTeamLeader: false,
-      avatar: '/me.png',
-    },
-    {
-      id: 3,
-      firstName: 'Liam',
-      lastName: 'Fuentes',
-      teamName: 'alpha',
-      isTeamLeader: false,
-    },
-    {
-      id: 4,
-      firstName: 'Tina',
-      lastName: 'Fey',
-      teamName: 'canary',
-      isTeamLeader: true,
-      avatar: '/me.png',
-    },
-    {
-      id: 5,
-      firstName: 'Katie',
-      lastName: 'Johnson',
-      teamName: 'canary',
-      isTeamLeader: false,
-    },
-    {
-      id: 6,
-      firstName: 'Tina',
-      lastName: 'Jones',
-      teamName: 'canary',
-      isTeamLeader: false,
-    },
-    {
-      id: 7,
-      firstName: 'Amy',
-      lastName: 'Adams',
-      teamName: 'delta',
-      isTeamLeader: true,
-    },
-    {
-      id: 8,
-      firstName: 'Ryan',
-      lastName: 'Lopez',
-      teamName: 'delta',
-      isTeamLeader: false,
-      avatar: '/me.png',
-    },
-    {
-      id: 9,
-      firstName: 'Jenny',
-      lastName: 'Jones',
-      teamName: 'delta',
-      isTeamLeader: false,
-    },
-  ]
+  await new Promise((resolve) => setTimeout(resolve, 1000))
   return (
     <Card>
       <CardHeader>
---
+++ data/empData.ts
@@ -0,0 +1,71 @@
+import { Employee } from "@/app/dashboard/employees/columns";
+
+export const employees: Employee[] = [
+  {
+    id: 1,
+    firstName: 'Colin',
+    lastName: 'Murray',
+    teamName: 'alpha',
+    isTeamLeader: true,
+    avatar: '',
+  },
+  {
+    id: 2,
+    firstName: 'Tom',
+    lastName: 'Phillips',
+    teamName: 'alpha',
+    isTeamLeader: false,
+    avatar: '/me.png',
+  },
+  {
+    id: 3,
+    firstName: 'Liam',
+    lastName: 'Fuentes',
+    teamName: 'alpha',
+    isTeamLeader: false,
+  },
+  {
+    id: 4,
+    firstName: 'Tina',
+    lastName: 'Fey',
+    teamName: 'canary',
+    isTeamLeader: true,
+    avatar: '/me.png',
+  },
+  {
+    id: 5,
+    firstName: 'Katie',
+    lastName: 'Johnson',
+    teamName: 'canary',
+    isTeamLeader: false,
+  },
+  {
+    id: 6,
+    firstName: 'Tina',
+    lastName: 'Jones',
+    teamName: 'canary',
+    isTeamLeader: false,
+  },
+  {
+    id: 7,
+    firstName: 'Amy',
+    lastName: 'Adams',
+    teamName: 'delta',
+    isTeamLeader: true,
+  },
+  {
+    id: 8,
+    firstName: 'Ryan',
+    lastName: 'Lopez',
+    teamName: 'delta',
+    isTeamLeader: false,
+    avatar: '/me.png',
+  },
+  {
+    id: 9,
+    firstName: 'Jenny',
+    lastName: 'Jones',
+    teamName: 'delta',
+    isTeamLeader: false,
+  },
+]
