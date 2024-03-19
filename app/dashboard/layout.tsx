a24ebc33c7356ddce545be584e34b86a13b7d641
arswe <abdurrahmansoftw@gmail.com>
2024-03-19 22:37:13 +0600

    React

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import MainMenu from '@/components/main-menu'
 import MenuTitle from '@/components/menu-title'
 import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer'
-import { useMediaQuery } from '@/hooks/useMediaQuery'
+import { useMediaQuery } from '@/hooks/use-media-query'
+
 import { MenuIcon } from 'lucide-react'
 import React from 'react'
 
@@ -11,17 +14,19 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='grid md:grid-cols-[250px_1fr] h-screen'>
       <MainMenu className='hidden md:flex' />
-      <div className='p-4 flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border'>
-        <MenuTitle />
-        <Drawer>
-          <DrawerTrigger>
-            <MenuIcon />
-          </DrawerTrigger>
-          <DrawerContent>
-            <MainMenu />
-          </DrawerContent>
-        </Drawer>
-      </div>
+      {!isDesktop && (
+        <div className='p-4 flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border'>
+          <MenuTitle />
+          <Drawer>
+            <DrawerTrigger>
+              <MenuIcon />
+            </DrawerTrigger>
+            <DrawerContent>
+              <MainMenu />
+            </DrawerContent>
+          </Drawer>
+        </div>
+      )}
 
       <div className='overflow-auto py-2 px-4'>
         <h1 className='pb-4'> Welcome back, AbdurRahman</h1>
