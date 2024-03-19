b7c267cde4de225346a68eb6d431abc80b4fde1c
arswe <abdurrahmansoftw@gmail.com>
2024-03-19 22:28:11 +0600

    Drawer

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import MainMenu from '@/components/main-menu'
 import MenuTitle from '@/components/menu-title'
+import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer'
+import { MenuIcon } from 'lucide-react'
 import React from 'react'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
@@ -8,7 +10,14 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <MainMenu className='hidden md:flex' />
       <div className='p-4 flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border'>
         <MenuTitle />
-        
+        <Drawer>
+          <DrawerTrigger>
+            <MenuIcon />
+          </DrawerTrigger>
+          <DrawerContent>
+            <MainMenu />
+          </DrawerContent>
+        </Drawer>
       </div>
 
       <div className='overflow-auto py-2 px-4'>
